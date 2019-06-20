45a511e19d98e689bc22add05ea5ef392a7312c9
mithun <mithunganesh1@gmail.com>
2019-06-20 14:50:38 +0530

    ResponseEntityChange

--- src/main/java/com/walmart/chile/ims/common/exception/WebRestControllerAdvice.java
+++ src/main/java/com/walmart/chile/ims/common/exception/WebRestControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.walmart.chile.ims.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,11 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class WebRestControllerAdvice {
   
   @ExceptionHandler(IMSException.class)
-  public IMSResponse handleNotFoundException(IMSException ex) {
+  public ResponseEntity<IMSResponse> handleNotFoundException(IMSException ex) {
 	  IMSResponse responseMsg = new IMSResponse();
 	  responseMsg.setErrorCode(ex.getErrorCode()+"");
 	  responseMsg.setMessage(ex.getErrorMessage());
 	  responseMsg.setStatus(ex.getErrorCode()+"");
-	  return responseMsg;
+	  return new ResponseEntity<IMSResponse>(responseMsg, HttpStatus.BAD_REQUEST);
   }
 }
